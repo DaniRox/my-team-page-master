131be0eb0457e2387461955ddefc49fce0f2ae71
DaniRox <daniroxescobar@gmail.com>
2023-09-20 22:57:52 -0300

    agregando funcion mediaqueries

--- mediaqueries.js
+++ mediaqueries.js
@@ -3,17 +3,31 @@ let item4 = document.getElementById("card-4");
 let item5 = document.getElementById("card-5");
 let item6 = document.getElementById("card-6");
 
-let mediaquery = window.matchMedia("(min-width: 612px)");
+let mediaqueryS = window.matchMedia("(max-width: 611px)");
+let mediaqueryL = window.matchMedia("(min-width: 612px)");
 
-console.log(mediaquery.matches);
+if (screen.width <= 611){
+    item4.classList.add("card-team--margin-top");
+    item6.classList.add("card-team--margin-top");
+    item5.classList.remove("card-team--margin-top");
+}else if (screen.width >= 612){
+    item5.classList.add("card-team--margin-top");
+    item4.classList.remove("card-team--margin-top");
+    item6.classList.remove("card-team--margin-top");
+}
 
-mediaquery.addEventListener('change', marginItem);
+mediaqueryS.addEventListener('change', marginItem);
 
-function marginItem(e){
-    if(e){
-        item5.classList.add("card-team--margin-top");
-    } else {
+function marginItem(){
+    if(mediaqueryS){
         item4.classList.add("card-team--margin-top");
         item6.classList.add("card-team--margin-top");
+        item5.classList.remove("card-team--margin-top");
     }
+    if(mediaqueryL){
+        item5.classList.add("card-team--margin-top");
+        item4.classList.remove("card-team--margin-top");
+        item6.classList.remove("card-team--margin-top");
+    }
+
 }
