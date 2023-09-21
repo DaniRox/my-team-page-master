216e578bb3c6abe8a2cc256281dfd530957a0d2d
DaniRox <daniroxescobar@gmail.com>
2023-09-20 23:20:03 -0300

    agregando funcion mediaqueries

--- mediaqueries.js
+++ mediaqueries.js
@@ -6,20 +6,24 @@ let item6 = document.getElementById("card-6");
 let mediaqueryS = window.matchMedia("(max-width: 611px)");
 let mediaqueryL = window.matchMedia("(min-width: 612px)");
 
+/*
+window.addEventListener('resize', marginItem);
+
 function marginItem(){
     if(mediaqueryL){
-        item5.classList.add("card-team--margin-top");
         item4.classList.remove("card-team--margin-top");
         item6.classList.remove("card-team--margin-top");
+        item5.classList.add("card-team--margin-top");
     }
     if(mediaqueryS){
+        item5.classList.remove("card-team--margin-top");
         item4.classList.add("card-team--margin-top");
         item6.classList.add("card-team--margin-top");
-        item5.classList.remove("card-team--margin-top");
     }
-}
 
-mediaqueryS.addEventListener('change', marginItem);
+
+}*/
+
 
 
 if (screen.width >= 612){
